5baca8a22b73f214872f2a5b27a0495fb95b6149
David Deepwell <ddeepwel@uwaterloo.ca>
2015-07-20 16:52:33 -0400

    added help to spins_plot2d

--- plot/spins_plot2d.m
+++ plot/spins_plot2d.m
@@ -1,8 +1,27 @@
 function pltinfo = spins_plot2d(var, t_index, varargin)
 % SPINS_PLOT2D  Plot cross-sections, averages or standard deviations of variables.
 %
-%   pltinfo = spins_plot2d(var,t_i) plots var at t_i
-%   other options
+%   pltinfo = spins_plot2d(var, t_i) plots var at t_i
+%   pltinfo = spins_plot2d(var, t_i, 'opt1', val1, ...) plots var at t_i with option 'opt1' as val1
+%   
+%   % Optional arguments:
+%	Name:	Options			- Description
+%       dimen:	{'X','Y','Z'}		- dimension to take cross-section
+%       slice:	{double}		- location to take cross-section
+%       style:  {'pcolor','contourf','contour'}
+%		- type of plot
+%	axis:	{[x1 x2 z1 z2]}		- domain to plot
+%	xskp:	{integer}		- x-grid points to skip in plot
+%	yskp:	{integer}		- y-grid     "
+%	zskp:	{integer}		- z-grid     "
+%       fnum:	{integer}		- figure window to make plot
+%	colorbar:  {boolean}		- plot colorbar?
+%	savefig:   {boolean}		- save figure in figure file?
+%	visible:   {boolean}		- make figure visible?
+%       ncontourf: {integer}		- contours to use for contourf plot
+%       ncontour:  {integer}		- contours to use for contour plot
+%	cont2:	{field name}		- secondary field to plot as contours
+%	ncont2:	{integer}		- contours to use for secondary field
 %
 %    David Deepwell, 2015
 global gdpar
