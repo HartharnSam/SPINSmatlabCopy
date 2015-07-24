1bcf7c9da0d92349c747e69835d68946ff226e2b
David Deepwell <ddeepwel@uwaterloo.ca>
2015-07-24 19:27:34 -0400

    fixed gridparams to handle mapped grids. started work on plotoptions to handle mapped grid started work on plot2dmapped

---
+++ plot/spins_plot2dmapped.m
@@ -0,0 +1,134 @@
+function pltinfo = spins_plot2dmapped(var, t_index, varargin)
+% SPINS_PLOT2DMAPPED  Plot cross-section, averages and standard deviations of variables from a mapped grid.
+%
+%   pltinfo = spins_plot2dmapped(var,t_i) plots var at t_i
+%   other options are listed in the help for spins_plot2d()
+%
+%    David Deepwell, 2015
+global gdpar
+
+    %% get grid, parameters
+gd = gdpar.gd;
+params = gdpar.params;
+    %% make sure grid is full
+gdnames = fieldnames(gd);
+if isvector(gd.(gdnames{1}))
+   error('spins_plot2d requires full grid for mapped grids.') 
+end
+    %% set plotting options
+spins_plotoptions
+
+    %% figure visibility options
+if p.Results.visible == false
+    figure('Visible','off'), clf
+else
+    figure(p.Results.fnum), clf
+end
+
+for ii = t_index
+    clf
+    hold on
+    % Title and axis labels
+    if strncmp(var,'Mean',4) || strncmp(var,'SD',2)
+        if isfield(params, 'plot_interval')
+            title(['Spanwise ',var,', t=',int2str(ii*params.plot_interval)])
+        else
+            title(['Spanwise ',var,', tn=',int2str(ii)])
+        end
+    elseif params.ndims == 2
+        if isfield(params, 'plot_interval')
+            title([var,', t=',int2str(ii*params.plot_interval)])
+        else
+            title([var,', tn=',int2str(ii)])
+        end
+    else
+        if isfield(params, 'plot_interval')
+            title([var,', ',p.Results.dimen,'=',num2str(cross_section),...
+                   ' m, t=',int2str(ii*params.plot_interval),' s'])
+        else
+            title([var,', ',p.Results.dimen,'=',num2str(cross_section),...
+                   ' m, tn=',int2str(ii)])
+        end
+    end
+    if length(nx)>1, xlabel('x (m)'), else xlabel('y (m)'), end
+    if length(nz)>1, ylabel('z (m)'), else ylabel('y (m)'), end
+
+    % get data to plot
+    [data1,primcol,cmap] = spins_readdata(var,ii,nx,ny,nz);
+
+    % choose plotting style (contourf may take up less memory,
+    % but can be slower than pcolor)
+    if strcmp(var,'Streamline')
+        prompt = 'Provide a sensible wave speed in m/s: ';
+        uwave = input(prompt);
+        disp(['background speed = ',num2str(uwave),' m/s'])
+        u1 = data1(:,:,1) - uwave;
+        u2 = data1(:,:,2);
+        streamslice(xvar,yvar,u1',u2',0.75)
+        cont2col = 'r-';
+    elseif strcmp(p.Results.style,'pcolor')
+        pcolor(xvar,yvar,data1')
+        cont2col = 'w-';
+    elseif strcmp(p.Results.style,'contourf')
+        contourf(xvar,yvar,data1',p.Results.ncontourf)
+        cont2col = 'w-';
+    elseif strcmp(p.Results.style,'contour')
+        contour(xvar,yvar,data1',p.Results.ncontour)
+        cont2col = 'k-';
+    end
+    % add extra information
+    shading flat
+    colormap(cmap)
+    caxis(primcol);
+    if p.Results.colorbar == true
+        colorbar
+    end
+    if ~strcmpi(p.Results.cont2,'None')      % add contours of another field
+        if strncmp(var,'Mean',4) || strncmp(var,'SD',2)
+            % choose Mean of field if primary field is Mean or SD
+            cont2 = ['Mean ',p.Results.cont2];
+        else
+            cont2 = p.Results.cont2;
+        end
+        if strcmp(cont2,var)            % read in data only if the field is different
+            data2 = data1;
+        else
+            [data2,~,~] = spins_readdata(cont2,ii,nx,ny,nz);
+            contour(xvar,yvar,data2',p.Results.ncont2,cont2col)
+        end
+    end
+    % axis options
+    if primaxis(2)/primaxis(4)>6 && (primaxis(2)-primaxis(1)>0.75)
+        axis normal
+    else
+        axis image
+    end
+    axis(primaxis)
+    set(gca,'layer','top')
+
+    % drawnow if plotting multiple outputs
+    if length(t_index) > 1, drawnow, end
+
+    % save figure
+    if p.Results.savefig == true
+        if ~(exist('figures','dir') == 7)
+            mkdir figures
+        end
+        cd figures
+        saveas(gcf,[filename,'.fig'],'fig');
+        cd('..')
+    end
+    hold off
+end
+
+% output plotted data
+pltinfo.xvar = xvar;
+pltinfo.yvar = yvar;
+pltinfo.data1 = data1;
+pltinfo.var1 = var;
+try
+    pltinfo.data2 = data2;
+    pltinfo.var2 = cont2;
+end
+pltinfo.dimen = p.Results.dimen;
+pltinfo.slice = cross_section;
--- support/spins_gridparams.m
+++ support/spins_gridparams.m
@@ -5,7 +5,10 @@ function gdpar = spins_gridparams(varargin)
 %   gdpar = spins_gridparams('Full')  gives the full grid and parameters in a structure
 %
 %   David Deepwell, 2015
-global gdpar    
+    global gdpar    
+
+    % Parser spins.conf
+    params = spins_params();
 
     % Read in grid
     if nargin > 1
@@ -17,33 +20,28 @@ global gdpar
             gd = spins_grid(varargin{1});
         end
     elseif nargin == 0
-        gd = spins_grid('Vector');
+        if strcmp(params.mapped_grid,'false')
+            gd = spins_grid('Vector');
+        %elseif strcmp(params.mapped_grid,'true') % default to give full grid if mapped grid
+        %    gd = spins_grid('Full');
+        end
     end
-    % Parser spins.conf
-    params = spins_params();
 
-    % Add other information, independent of mapping, into params object
-    [new_params, gdvec] = add_params(gd, params);
+    % Add other information into params structure
+    par = add_params(gd, params);
 
-    % Add other information, dependent on mapping
-    if strcmpi(params.mapped_grid, 'true')
-        par = add_mapped_params(gd, new_params, gdvec);
-    elseif strcmpi(params.mapped_grid, 'false')
-        par = add_unmapped_params(gdvec, new_params);
-    end
-    
-    % Place information into a structure
+    % Place information into another structure
     gdpar.gd = gd;
     gdpar.params = par;
 end
 
-function [par, gdvec] = add_params(gd, params)
+function par = add_params(gd, params)
     % Number of dimensions
     gdnames = fieldnames(gd);
     params.ndims = length(gdnames);
 
     % check if grid is vectorized
-    if isvector(getfield(gd, gdnames{1}))
+    if isvector(gd.(gdnames{1}))
        gdvec = gd;
     else  % get vectorized grid
         gdvec = get_vector_grid(gd, params);
@@ -121,141 +119,66 @@ function [par, gdvec] = add_params(gd, params)
         end
     end 
 
-    par = params;
-end
-
-    
-function par = add_unmapped_params(gd, p)
-    % grab vectorized grids
-    try 
-        x1d = gd.x;
-    end
-    try 
-        y1d = gd.y;
-    end
-    try 
-        z1d = gd.z;
-    end
-
-    % Length of domain
-    if ~isfield(p, 'Lx') && isfield(gd, 'x')
-        if strcmp(p.type_x,'NO_SLIP') || strcmp(p.type_x,'CHEBY')
-            p.Lx = roundn(abs(x1d(end)-x1d(1)), -15);
+    % Length of domain (assumes topography only affects the z-coordinate)
+    if ~isfield(params, 'Lx') && isfield(gd, 'x')
+        if strcmp(params.type_x,'NO_SLIP') || strcmp(params.type_x,'CHEBY')
+            params.Lx = roundn(abs(x1d(end)-x1d(1)), -15);
         else
-            p.Lx = roundn(p.Nx*p.dx, -15);
+            params.Lx = roundn(Nx*dx, -15);
         end
     end
-    if ~isfield(p,'Ly') && isfield(gd,'y')
-        if strcmp(p.type_y,'NO_SLIP') || strcmp(p.type_y,'CHEBY')
-            p.Ly = roundn(abs(y1d(end)-y1d(1)), -15);
+    if ~isfield(params,'Ly') && isfield(gd,'y')
+        if strcmp(params.type_y,'NO_SLIP') || strcmp(params.type_y,'CHEBY')
+            params.Ly = roundn(abs(y1d(end)-y1d(1)), -15);
         else
-            p.Ly = roundn(p.Ny*p.dy, -15);
+            params.Ly = roundn(Ny*dy, -15);
         end
     end
-    if ~isfield(p,'Lz') && isfield(gd,'z')
-        if strcmp(p.type_z,'NO_SLIP') || strcmp(p.type_z,'CHEBY')
-            p.Lz = roundn(abs(z1d(end)-z1d(1)), -15);
+    if ~isfield(params,'Lz') && isfield(gd,'z')
+        if strcmp(params.type_z,'NO_SLIP') || strcmp(params.type_z,'CHEBY')
+            params.Lz = roundn(max(gd.z(:)) - min(gd.z(:)), -15);
         else
-            p.Lz = roundn(p.Nz*p.dz, -15);
+            params.Lz = roundn(Nz*dz, -15);
         end
     end
 
     % Bounds of domain
-    if ~isfield(p,'xlim') && isfield(gd,'x')
-        if strcmp(p.type_x,'NO_SLIP') || strcmp(p.type_x,'CHEBY')
+    if ~isfield(params,'xlim') && isfield(gd,'x')
+        if strcmp(params.type_x,'NO_SLIP') || strcmp(params.type_x,'CHEBY')
             x1 = min(x1d(1), x1d(end));
             x2 = max(x1d(1), x1d(end));
         else
-            x1 = min(x1d(1), x1d(end)) - p.dx/2;
-            x2 = max(x1d(1), x1d(end)) + p.dx/2;
+            x1 = min(x1d(1), x1d(end)) - dx/2;
+            x2 = max(x1d(1), x1d(end)) + dx/2;
         end
         xL = roundn(x1, -15);
         xR = roundn(x2, -15);
-        p.xlim = [xL, xR];
+        params.xlim = [xL, xR];
     end
-    if ~isfield(p,'ylim') && isfield(gd,'y')
-        if strcmp(p.type_y,'NO_SLIP') || strcmp(p.type_y,'CHEBY')
+    if ~isfield(params,'ylim') && isfield(gd,'y')
+        if strcmp(params.type_y,'NO_SLIP') || strcmp(params.type_y,'CHEBY')
             y1 = min(y1d(1), y1d(end));
             y2 = max(y1d(1), y1d(end));
         else
-            y1 = min(y1d(1), y1d(end)) - p.dy/2;
-            y2 = max(y1d(1), y1d(end)) + p.dy/2;
+            y1 = min(y1d(1), y1d(end)) - dy/2;
+            y2 = max(y1d(1), y1d(end)) + dy/2;
         end
         yL = roundn(y1, -15);
         yR = roundn(y2, -15);
-        p.ylim = [yL, yR];
+        params.ylim = [yL, yR];
     end
-    if ~isfield(p,'zlim') && isfield(gd,'z')
-        if strcmp(p.type_z,'NO_SLIP') || strcmp(p.type_z,'CHEBY')
-            z1 = min(z1d(1), z1d(end));
-            z2 = max(z1d(1), z1d(end));
+    if ~isfield(params,'zlim') && isfield(gd,'z')
+        if strcmp(params.type_z,'NO_SLIP') || strcmp(params.type_z,'CHEBY')
+            z1 = min(gd.z(:));
+            z2 = max(gd.z(:));
         else
-            z1 = min(z1d(1), z1d(end)) - p.dz/2;
-            z2 = max(z1d(1), z1d(end)) + p.dz/2;
+            z1 = min(z1d(1), z1d(end)) - dz/2;
+            z2 = max(z1d(1), z1d(end)) + dz/2;
         end
         zL = roundn(z1, -15);
         zR = roundn(z2, -15);
-        p.zlim = [zL, zR];
+        params.zlim = [zL, zR];
     end
 
-    par = p;
-end
-
-function par = add_mapped_params(gd, p, gdvec)    
-    % grab vectorized grids
-    try 
-        x1d = gdvec.x;
-    end
-    try 
-        y1d = gdvec.y;
-    end
-    try 
-        z1d = gdvec.z;
-    end
-
-    % Length of domain
-    if ~isfield(p, 'Lx') && isfield(gd, 'x')
-        if strcmp(p.type_x,'NO_SLIP') || strcmp(p.type_x,'CHEBY')
-            p.Lx = roundn(abs(x1d(end)-x1d(1)), -15);
-        else
-            p.Lx = roundn(p.Nx*p.dx, -15);
-        end
-    end
-    if ~isfield(p,'Ly') && isfield(gd,'y')
-        if strcmp(p.type_y,'NO_SLIP') || strcmp(p.type_y,'CHEBY')
-            p.Ly = roundn(abs(y1d(end)-y1d(1)), -15);
-        else
-            p.Ly = roundn(p.Ny*p.dy, -15);
-        end
-    end
-    if ~isfield(p,'Lz') && isfield(gd,'z')
-keyboard
-        if strcmp(p.type_z,'NO_SLIP') || strcmp(p.type_z,'CHEBY')
-            p.Lz = roundn(max(gd.z(:)) - min(gd.z(:)), -15);
-        else
-            p.Lz = roundn(p.Nz*p.dz, -15);
-        end
-    end
-
-    par = p;
-end
-
-function gdvec = get_vector_grid(gd, params)
-    if params.ndims == 3
-        gdvec.x = gd.x(:,1,1);
-        gdvec.y = gd.y(1,:,1);
-        gdvec.z = gd.z(1,1,:);
-    else
-        if isfield(gd, 'x')
-            gdvec.x = gd.x(:,1);
-            try
-                gdvec.y = gd.y(1,:);
-            catch
-                gdvec.z = gd.z(1,:);
-            end
-        else
-            gdvec.y = gd.y(:,1);
-            gdvec.z = gd.z(1,:);
-        end
-    end
+    par = params;
 end
