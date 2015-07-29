1dbb4428ba97cab58b8185d3d0d3598a64e14f55
David Deepwell <ddeepwel@uwaterloo.ca>
2015-07-28 21:21:53 -0400

    added a little more

--- support/get_plot_points.m
+++ support/get_plot_points.m
@@ -59,3 +59,56 @@ function [nx, ny, nz, xvar, yvar] = unmapped_points(gd, params, cross_section, p
             xvarR = nearestindex(y, primaxis(2));
             if isvector(z)
                 yvarB = nearestindex(z, primaxis(3));
+
+
+
+% find grid points to read in
+if strcmp(p.Results.dimen,'X')		% X dimen
+    if length(p.Results.axis) == 1		% no axis flag
+        ny = 1:p.Results.yskp:Ny;
+        nz = 1:p.Results.zskp:Nz;
+        primaxis=[ylimits zlimits]; % the plot area
+    else					% with axis flag
+        primaxis = p.Results.axis;
+        if primaxis(2)<= primaxis(1) || primaxis(4)<=primaxis(3)
+            error('Axis must be ordered correctly.')
+        end
+        xvarL = nearestindex(y, primaxis(1));
+        xvarR = nearestindex(y, primaxis(2));
+        yvarB = nearestindex(z, primaxis(3));
+        yvarT = nearestindex(z, primaxis(4));
+        xvarL2 = min(xvarL,xvarR); xvarR2 = max(xvarL,xvarR);
+        yvarB2 = min(yvarB,yvarT); yvarT2 = max(yvarB,yvarT);
+        ny = xvarL2:p.Results.xskp:xvarR2;
+        nz = yvarB2:p.Results.zskp:yvarT2;
+    end
+    if params.ndims == 3
+        nx = nearestindex(x, cross_section);
+    else
+        nx = 1;
+    end
+    xvar = y(ny);
+    yvar = z(nz);
+elseif strcmp(p.Results.dimen,'Y')	% Y dimen
+    if length(p.Results.axis) == 1		% no axis flag
+        nx = 1:p.Results.xskp:Nx;
+        nz = 1:p.Results.zskp:Nz;
+        primaxis = [xlimits zlimits]; % the plot area
+    else					% with axis flag
+        primaxis = p.Results.axis;
+        if primaxis(2)<= primaxis(1) || primaxis(4)<=primaxis(3)
+            error('Axis must be ordered correctly.')
+        end
+        xvarL = nearestindex(x, primaxis(1));
+        xvarR = nearestindex(x, primaxis(2));
+        yvarB = nearestindex(z, primaxis(3));
+        yvarT = nearestindex(z, primaxis(4));
+        xvarL2 = min(xvarL,xvarR); xvarR2 = max(xvarL,xvarR);
+        yvarB2 = min(yvarB,yvarT); yvarT2 = max(yvarB,yvarT);
+        nx = xvarL2:p.Results.xskp:xvarR2;
+        nz = yvarB2:p.Results.zskp:yvarT2;
+    end
+    if params.ndims == 3
+        ny = nearestindex(y, cross_section);
+    else
+        ny = 1;
