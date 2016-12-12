72f318a912aa3dd7aa262bc91e89544845f05a6e
David Deepwell <ddeepwel@uwaterloo.ca>
2016-12-11 20:13:55 -0500

    added the simple function to see what the first SPINS output is in the current directory

---
+++ first_output.m
@@ -0,0 +1,28 @@
+function num = first_output()
+% FIRST_OUTPUT     Find the first SPINS output in the working directory
+%
+%  Usage:
+%    num = first_output()
+%
+%  Inputs:
+%    -none
+%
+%  Outputs:
+%    'num' - the number of the first output
+%
+%  David Deepwell, 2016
+
+    % use the u field to find the smallest output number
+    files = dir('u.*');
+    nfiles = length(files);
+    table = struct2dataset(files);
+
+    % find all extension numbers
+    outputs = zeros(1,nfiles);
+    for ii = 1:nfiles
+        filename = table{ii,1};
+        [~, dot_num] = strtok(filename, '.');
+        outputs(ii) = str2num(dot_num(2:end));
+    end
+    num = min(outputs);
+end
