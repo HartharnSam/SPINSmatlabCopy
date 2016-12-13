d88314c7204d63827264e950ac0d5d40c9a76650
David Deepwell <ddeepwel@uwaterloo.ca>
2016-12-12 20:07:04 -0500

    Created function which holds all test strings to creating labels for subplot. Returns a cell array of {'a)','b)','c)',...}

---
+++ subplot_labels.m
@@ -0,0 +1,9 @@
+function labels = subplot_labels()
+% Return a cell array of strings of the alphabet. 
+% Useful for adding labels to subplots
+%
+% David Deepwell, 2016
+
+labels = {'a)','b)','c)','d)','e)','f)','g)','h)','i)',...
+          'j)','k)','l)','m)','n)','o)','p)','q)','r)',...
+          's)','t)','u)','v)','w)','x)','y)','z)'};
