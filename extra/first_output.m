17058c27bae16ce035fa32589b6008d67472d6be
David Deepwell <ddeepwel@uwaterloo.ca>
2017-01-26 14:27:24 -0500

    Made first_output.m more general.
    first_output.m now ignores all non-integer extensions.

--- extra/first_output.m
+++ extra/first_output.m
@@ -13,7 +13,7 @@ function num = first_output(var)
 %
 %  David Deepwell, 2016
 
-    % use the u field to find the smallest output number
+    % use the field to find the smallest output number
     if exist('var','var')
         files = dir([var,'.*']);
     else
@@ -22,15 +22,16 @@ function num = first_output(var)
     nfiles = length(files);
     table = struct2dataset(files);
 
-    % find all extension numbers
+    % find all extensions
     outputs = zeros(1,nfiles);
     for ii = 1:nfiles
         filename = table{ii,1};
         [~, dot_num] = strtok(filename, '.');
-        if strcmp(dot_num, '.dump')
+        num_str = str2num(dot_num(2:end));
+        if isempty(num_str)
             outputs(ii) = NaN;
         else
-            outputs(ii) = str2num(dot_num(2:end));
+            outputs(ii) = str2num(num_str);
         end
     end
     num = min(outputs);
