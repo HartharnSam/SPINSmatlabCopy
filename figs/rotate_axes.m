9b808288e6d9315bb7a5803d6f926a865d645fca
David Deepwell <ddeepwel@uwaterloo.ca>
2018-10-16 15:37:31 -0600

    Add a function to rotate an axis
    Useful for simulations of a tilted tank.

---
+++ figs/rotate_axes.m
@@ -0,0 +1,22 @@
+function [] = rotate_axes(fig_hand, theta);
+% Rotate the axes object by some angle theta
+%
+% David Deepwell, 2018
+
+% get the correct figure
+if nargin > 0
+    figure(fig_hand); % use provided figure handle
+end
+% if not, use the current figure
+ax = gca;
+
+% If no angle specified, use the one in the spins.conf
+if nargin < 2
+    par = spins_params();
+    theta = par.tilt_angle;
+end
+
+% rotate axes
+UpVector = [-sind(theta), cosd(theta), 0];
+DAR = get(ax, 'DataAspectRatio');
+set(ax, 'CameraUpVector', DAR.*UpVector);
