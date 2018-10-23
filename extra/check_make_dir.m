7e043bc6e0bd697eaa743b6d6317b0570cb093a4
David Deepwell <ddeepwel@uwaterloo.ca>
2018-10-23 16:34:03 -0400

    Fix bug in check_make_dir
    The directory used to be checked for existence over the entire
    path, not just the local one. Now checked on the local directory
    only.

--- extra/check_make_dir.m
+++ extra/check_make_dir.m
@@ -1,22 +1,7 @@
-function check_make_dir(the_path)
+function check_make_dir(direc)
 % CHECK_MAKE_DIR  Check if path exists in local directory
-%                 and make the path if it doesn't exist
+%                 and make the directory(ies) if it doesn't
 
-orig_dir = pwd();
-
-% separate path into sub-directories
-dirs = strsplit(the_path,'/');
-% remove empty strings
-dirs(strcmp('',dirs)) = [];
-
-% loop over sub-directories
-for nn = 1:length(dirs)
-    direc = dirs{nn};
-    if ~(exist(direc,'dir')==7)
-        mkdir(direc);
-    end
-    cd(direc);
+if ~(exist([pwd,'/',direc],'dir')==7)
+    mkdir(direc);
 end
-
-% return to original directory
-cd(orig_dir);
