f462af7054e55fd0f35781e8dfeefabb401bec39
David Deepwell <ddeepwel@uwaterloo.ca>
2018-10-24 17:11:45 -0400

    Add optional argument to subplot_labels
    Use of optional argument will return the label for the particular
    integer input.

--- figs/subplot_labels.m
+++ figs/subplot_labels.m
@@ -1,9 +1,15 @@
-function labels = subplot_labels()
+function labels = subplot_labels(nn)
 % Return a cell array of strings of the alphabet. 
-% Useful for adding labels to subplots
+% Useful for adding labels to subplots.
+% If an integer is passed as an argument, then the label
+% associated with that integer is returned.
 %
 % David Deepwell, 2018
 
 labels = {'(a)','(b)','(c)','(d)','(e)','(f)','(g)','(h)','(i)',...
           '(j)','(k)','(l)','(m)','(n)','(o)','(p)','(q)','(r)',...
           '(s)','(t)','(u)','(v)','(w)','(x)','(y)','(z)'};
+
+if nargin == 1
+    labels = labels{nn};
+end
