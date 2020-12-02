f171b87448fee7a077e9a1240c9a5b981b7dfc5c
HartharnSam <57717460+HartharnSam@users.noreply.github.com>
2020-12-02 15:49:40 +0000

    re-fixed spinsstartup to still work on windows!

--- spinsstartup.m
+++ spinsstartup.m
@@ -9,8 +9,11 @@
 
 sm_loc = genpath(['C:\Users\', getenv('username'), '\OneDrive - Newcastle University\Project\Shoal_Core\scripts\Model\SPINSmatlab']);
 addpath(sm_loc);
-
-all_subs =  strsplit(sm_loc,':');
+if ispc
+    all_subs =  strsplit(sm_loc,';');
+else
+    all_subs = strsplit(sm_loc, ':');
+end
 for ii = 1:length(all_subs)
     if ~isempty(strfind(all_subs{ii},'.'))
         rmpath(all_subs{ii});
@@ -20,7 +23,11 @@ end
 %% Add SPINS_main/matlab to path
 sm_matlab_loc = genpath(['C:\Users\', getenv('username'), '\OneDrive - Newcastle University\Project\Shoal_Core\scripts\Model\SPINS_main\matlab']);
 addpath(sm_matlab_loc);
-all_subs =  strsplit(sm_matlab_loc,':');
+if ispc
+    all_subs =  strsplit(sm_matlab_loc,';');
+else
+    all_subs = strsplit(sm_matlab_loc, ':');
+end
 for ii = 1:length(all_subs)
     if ~isempty(strfind(all_subs{ii},'.'))
         rmpath(all_subs{ii});
