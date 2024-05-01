07c5de9259103b720b62bd92296098c7268a1dcf
Sam Hartharn-Evans <s.hartharn-evans2@ncl.ac.uk>
2024-05-01 14:02:23 +0100

    Update slice_grids to function

--- IO/slice_grids.m
+++ IO/slice_grids.m
@@ -1,7 +1,10 @@
+function [xv, zv, xh, yh, yp, zp] = slice_grids(x, y, z)
 % This M-file creates the various 2D grids for 2d slices in SPINS
 xv=xzslice(x,1);
 zv=xzslice(z,1);
-xh=xyslice(x,1);
-yh=xyslice(y,1);
-yp=yzslice(y,1);
-zp=yzslice(z,1);
+if nargin > 2
+    xh=xyslice(x,1);
+    yh=xyslice(y,1);
+    yp=yzslice(y,1);
+    zp=yzslice(z,1);
+end
